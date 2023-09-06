7b9a3b95e98aa8594838922c93009c09ad1b8f7e
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-09-06 20:05:47 +0530

    Create 04.cpp

---
+++ Monthly_Challenges/Sep/04.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/linked-list-cycle/?envType=daily-question&envId=2023-09-04
+class Solution {
+public:
+
+bool hasCycle(ListNode *head) {
+        // ListNode*fast=head;
+        //  ListNode* slow=head;
+        //  while(fast!=NULL && fast->next!=NULL)
+        //  {
+        //      fast=fast->next->next;
+        //      slow=slow->next;
+        //      if(slow==fast)
+        //       return true;
+        //  }
+        //  return false;
+       
+    }
+};
