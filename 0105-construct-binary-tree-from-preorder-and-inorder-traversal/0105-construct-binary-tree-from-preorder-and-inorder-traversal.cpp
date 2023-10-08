1c4172525a34112f8716aea184e2f0b6a7a92fc8
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-08 16:01:26 +0530

    Time: 23 ms (40.54%), Space: 26.1 MB (75.76%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,27 +10,32 @@
  * };
  */
 class Solution {
-public:
-    int ind = 0;
+    private:
+    int f_position(vector<int>&inorder,int in_start,int in_end,int element,int n){
+        for(int i=in_start;i<=in_end;i++){
+            if(inorder[i]==element){
+                return i;
+            }
 
-    TreeNode* f(vector<int>& preorder, vector<int>& inorder, int start, int end) {
-        if (start > end) {
+        }
+        return -1;
+    }
+    TreeNode* f(vector<int>&preorder,vector<int>&inorder,int &index,int in_start,int in_end,int n){
+        if(index>n || in_start>in_end){
             return nullptr;
         }
-        TreeNode* root = new TreeNode(preorder[ind++]);
-        int sp = 0;
-        for (int i = start; i <= end; i++) {
-            if (inorder[i] == preorder[ind - 1]) { 
-                sp = i;
-                break;
-            }
-        }
-        root->left = f(preorder, inorder, start, sp - 1);
-        root->right = f(preorder, inorder, sp + 1, end);
+        int element=preorder[index++];
+        TreeNode* root=new TreeNode(element);
+        int position=f_position(inorder,in_start,in_end,element,n);
+        root->left=f(preorder,inorder,index,in_start,position-1,n);
+        root->right=f(preorder,inorder,index,position+1,in_end,n);
         return root;
     }
-
+public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return f(preorder, inorder, 0, inorder.size() - 1);
+        int n=inorder.size();
+        int pre_index=0;
+        TreeNode* ans=f(preorder,inorder,pre_index,0,n-1,n);
+        return ans;
     }
 };
