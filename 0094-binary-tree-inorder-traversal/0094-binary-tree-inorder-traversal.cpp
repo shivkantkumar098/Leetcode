d69e48d835df36f94e7671e822b42f79c81ab8fd
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-09 19:18:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.09%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,35 +12,30 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*>st;
-        vector<int>res;
-        while(!st.empty()||root!=nullptr){
-            if(root!=nullptr){
-                st.push(root);
-                root=root->left;
-            }
-            else{
-                if(st.empty()) break;
-                root=st.top();
-                st.pop();
-                res.push_back(root->val);
-                root=root->right;
-            }
-        }
-        return res;
-        // while(!st.empty()|| root!=NULL){
-        //     if(root!=NULL){
-        //         st.push(root);
-        //         root=root->left;
-        //     }
-        //     else{
-        //         if(st.empty()) break;
-        //         root=st.top();
-        //         st.pop();
-        //         res.push_back(root->val);
-        //         root=root->right;
-        //     }
-        // }
-        // return res;
+
+       vector<int>in;
+       TreeNode*curr=root;
+       while(curr!=nullptr){
+           if(curr->left==nullptr){
+               in.push_back(curr->val);
+               curr=curr->right;
+           }
+           else{
+               TreeNode *prev=curr->left;
+               while(prev->right!=nullptr && prev->right!=curr){
+                   prev=prev->right;
+               }
+               if(prev->right==nullptr){
+                   prev->right=curr;
+                   curr=curr->left;
+               }
+               else{
+                   prev->right=nullptr;
+                   in.push_back(curr->val);
+                   curr=curr->right;
+               }
+           }
+       } 
+       return in;
     }
 };
