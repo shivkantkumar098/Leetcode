5d129d622791959df75d33beed461fce71038a37
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-11-11 13:23:53 +0530

    Time: 3 ms (48.21%), Space: 6.9 MB (23.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -7,9 +7,9 @@ public:
         // Start from the end of the string
         for (int i = n - 1; i >= 0; i--) {
             // Skip trailing spaces
-            if (s[i] == ' ' && cnt == 0) {
-                continue;
-            }
+            // if (s[i] == ' ' && cnt == 0) {
+            //     continue;
+            // }
             
             // If a non-space character is encountered, increment the count
             if (s[i] != ' ') {
