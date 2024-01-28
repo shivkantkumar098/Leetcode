a8ffcc8a0f14e6979c321117959460384966e57f
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2024-01-29 00:52:40 +0530

    Update 0002-add-two-numbers.cpp

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,7 +14,7 @@ public:
         ListNode *dummy=new ListNode();
         ListNode *temp=dummy;
         int carry=0;
-        while(l1!=NULL || l2!=NULL||carry){
+        while(l1!=NULL||l2!=NULL||carry){
             int sum=0;
             if(l1!=NULL){
                 sum+=l1->val;
