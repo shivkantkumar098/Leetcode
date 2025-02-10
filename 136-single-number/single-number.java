a37fd61d1e725d93ac6d71ecf98baa972365cd50
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-02-10 10:41:23 +0530

    Time: 14 ms (13.19%) | Memory: 45.3 MB (88.98%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -3,11 +3,10 @@ class Solution {
         HashMap<Integer,Integer>mp=new HashMap<>();
         for(int i:nums){
             mp.put(i,mp.getOrDefault(i,0)+1);
-
-        }
+        };
         for(int key:mp.keySet()){
             if(mp.get(key)==1){
-               return key;
+                return key;
             }
         }
         return -1;
