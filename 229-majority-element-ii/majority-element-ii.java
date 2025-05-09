541b96079c4c73d28e59aa9706c83348cb703b64
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-05-09 10:09:17 +0530

    Time: 3 ms (72.13%) | Memory: 50.6 MB (5.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer>mp=new HashMap<>();
-        List<Integer>res=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-        // int value=mp.getOrDefault(nums[i],0);
-        mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-     }   
-     for(Map.Entry<Integer,Integer>it:mp.entrySet()){
-        if(it.getValue()>nums.length/3){
-            
-            res.add(it.getKey());
+         int n = nums.length; //size of the array
+
+        int cnt1 = 0, cnt2 = 0; // counts
+        int el1 = Integer.MIN_VALUE; // element 1
+        int el2 = Integer.MIN_VALUE; // element 2
+
+        // applying the Extended Boyer Moore's numsoting Algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+            else {
+                cnt1--; cnt2--;
+            }
         }
-     }
-     return res;
+
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
+        }
+
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) ls.add(el1);
+        if (cnt2 >= mini) ls.add(el2);
+        return ls;
     }
 }
