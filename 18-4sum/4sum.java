64c23f6cb2bc50727b0ddbd088c3d2e8066c68b7
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-05-09 08:56:52 +0530

    Time: 19 ms (41.37%) | Memory: 43.9 MB (78.45%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,33 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
         List<List<Integer>>res=new ArrayList<>();
-        if(n<4)return res;
+        int n=nums.length;
         Arrays.sort(nums);
         for(int i=0;i<n-3;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
+            if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1;j<n-2;j++){
-                if(j>i+1&& nums[j]==nums[j-1])continue;
+                if(j>i+1 && nums[j]==nums[j-1])continue;
                 int low=j+1,high=n-1;
                 while(low<high){
                     long sum=(long)nums[i]+nums[j]+nums[low]+nums[high]; 
                     if(sum==target){
-
-
-                        res.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[low],nums[high])));
+                        res.add(Arrays.asList(nums[i],nums[j],nums[low],nums[high]));
                         while(low<high && nums[low]==nums[low+1])low++;
                         while(low<high && nums[high]==nums[high-1])high--;
                         low++;
                         high--;
                     }
-                    else if(sum>target){
-                        high--;
-                    }
-                    else{
-                        low++;
-                    }
+                    else if(sum<target)low++;
+                    else high--;
                 }
             }
+
         }
         return res;
     }
