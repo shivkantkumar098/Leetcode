baa822e35c88e5fa3cd347e17aa79cf3f05903f8
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-05-21 08:23:36 +0530

    Time: 2 ms (93.16%) | Memory: 47.5 MB (25.68%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
-        // int i=0,j=numbers.length-1;
-        // while(i<j){
-        //     int sum=numbers[i]+numbers[j];
-        //     if(sum==target){
-        //         return new int[]{i+1,j+1};
-        //     }
-        //     else if(sum<target){
-        //         i++;
-        //     }
-        //     else{
-        //         j--;
-        //     }
-        // }
-        // return new int[]{};
-        int n = numbers.length;
-        HashMap<Integer, Integer> mp = new HashMap<>(n);
-        
-        for (int i = 0; i < n; i++) {
-            int complement = target - numbers[i];
-            if (mp.containsKey(complement)) {
-                return new int[]{mp.get(complement) + 1, i + 1}; // 1-based indices
+        int i=0,j=numbers.length-1;
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                return new int[]{i+1,j+1};
             }
-            mp.put(numbers[i], i);
+            else if(sum<target){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
-        return new int[0]; 
+        return new int[]{};
+        // int n = numbers.length;
+        // HashMap<Integer, Integer> mp = new HashMap<>(n);
+        
+        // for (int i = 0; i < n; i++) {
+        //     int complement = target - numbers[i];
+        //     if (mp.containsKey(complement)) {
+        //         return new int[]{mp.get(complement) + 1, i + 1}; // 1-based indices
+        //     }
+        //     mp.put(numbers[i], i);
+        // }
+        // return new int[0]; 
     }
 }
