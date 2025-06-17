6f09dda218c4beb3e495d1a3fef67aff71ec2bad
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-06-17 11:10:51 +0530

    Time: 5 ms (69.01%) | Memory: 44.1 MB (62.21%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        int ori=x;
+        // this is for negative TC
         if(x<0)return false;
-     int org=x;
-     int no=0;
-     while(x!=0){
-        int rem=x%10;
-        no=no*10+rem;
-        x/=10;
-     }  
-     if(no==org){
-        return true;
-     } 
-     else{
-        return false;
-     }
+        int rev=0;
+        while(x!=0){
+            int d=x%10;
+            rev=rev*10+d;
+            x/=10;
+        }
+        if(ori!=rev)return false;
+        else return true;
     }
 }
