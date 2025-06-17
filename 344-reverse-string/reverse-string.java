bf32d64105b028691170a6f46dfca5e1f07825cc
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-06-17 11:02:56 +0530

    Time: 6 ms (4.03%) | Memory: 51.3 MB (5.04%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -10,13 +10,23 @@ class Solution {
         //     ans[i]=s[i];
         // }
 
-        int i=0,j=n-1;
-        while(i<=j){
-            char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
-            i++;
-            j--;
-        }
+        // int i=0,j=n-1;
+        // while(i<=j){
+        //     char temp=s[i];
+        //     s[i]=s[j];
+        //     s[j]=temp;
+        //     i++;
+        //     j--;
+        // }
+         List<Character> list = IntStream.range(0, s.length)
+            .mapToObj(i -> s[i])
+            .collect(Collectors.toList());
+
+        Collections.reverse(list); // Reverse using Collections API
+
+        // Copy back to original array
+        for (int i = 0; i < n; i++) {
+            s[i] = list.get(i);
+    }
     }
 }
