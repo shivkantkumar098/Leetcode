b8f69912bc3e45b73fd003bdedf00d0ab70828b0
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-06-24 16:40:48 +0530

    Time: 3 ms (75.64%) | Memory: 50.5 MB (35.99%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,35 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-         int n = nums.length; //size of the array
+     List<Integer>res=new ArrayList<>();
+        int cnt1=0,cnt2=0;
+        int n=nums.length;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+        if(cnt1==0 && el2!=nums[i]){
+            cnt1=1;
+            el1=nums[i];
 
-        int cnt1 = 0, cnt2 = 0; // counts
-        int el1 = Integer.MIN_VALUE; // element 1
-        int el2 = Integer.MIN_VALUE; // element 2
-
-        // applying the Extended Boyer Moore's numsoting Algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && el2 != nums[i]) {
-                cnt1 = 1;
-                el1 = nums[i];
-            } else if (cnt2 == 0 && el1 != nums[i]) {
-                cnt2 = 1;
-                el2 = nums[i];
-            } else if (nums[i] == el1) cnt1++;
-            else if (nums[i] == el2) cnt2++;
-            else {
-                cnt1--; cnt2--;
-            }
         }
-
-        List<Integer> ls = new ArrayList<>(); // list of answers
-
-        // Manually check if the stored elements in
-        // el1 and el2 are the majority elements:
-        cnt1 = 0; cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el1) cnt1++;
-            if (nums[i] == el2) cnt2++;
+        else if(cnt2==0 && el1!=nums[i]){
+            cnt2=1;
+            el2=nums[i];
         }
-
-        int mini = (int)(n / 3) + 1;
-        if (cnt1 >= mini) ls.add(el1);
-        if (cnt2 >= mini) ls.add(el2);
-        return ls;
+        else if(nums[i]==el1)cnt1++;
+        else if(nums[i]==el2)cnt2++;
+        else{
+            cnt1--;
+            cnt2--;
+        }
+        }
+        cnt1=0;cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1)cnt1++;
+            if(nums[i]==el2)cnt2++;
+        }
+        int mini=(int)(n/3)+1;
+        if(cnt1>=mini)res.add(el1);
+        if(cnt2>=mini)res.add(el2);
+        return res;
     }
 }
