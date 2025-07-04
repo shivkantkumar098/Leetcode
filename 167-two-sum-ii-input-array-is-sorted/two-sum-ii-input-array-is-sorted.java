e0c4c1c1ff3298c88f608c682fc6174a160ffacb
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-07-04 10:35:21 +0530

    Time: 6 ms (15.06%) | Memory: 46.6 MB (97.17%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int n=numbers.length;
-        int i=0,j=n-1;
-        while(i<j){
-            int sum=numbers[i]+numbers[j];
-            if(sum==target){
-                return new int[]{i+1,j+1};
-            }
-            else if(sum<target){
-                i++;
-            }else{
-                j--;
-            }
+        // int i=0,j=n-1;
+        // while(i<j){
+        //     int sum=numbers[i]+numbers[j];
+        //     if(sum==target){
+        //         return new int[]{i+1,j+1};
+        //     }
+        //     else if(sum<target){
+        //         i++;
+        //     }else{
+        //         j--;
+        //     }
           
+        // }
+        // return new int[]{-1,-1};
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<numbers.length;i++){
+            int complement=target-numbers[i];
+            if(mp.containsKey(complement)){
+                return new int[]{mp.get(complement)+1,i+1};
+            }
+            mp.put(numbers[i],i);
         }
         return new int[]{-1,-1};
 
