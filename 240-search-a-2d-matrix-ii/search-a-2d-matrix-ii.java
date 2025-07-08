9352e723fca64b79af0340399a8cfda2791f6bce
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2025-07-08 17:57:55 +0530

    Time: 6 ms (48.15%) | Memory: 46.2 MB (38.30%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return false;
+    }
+}
